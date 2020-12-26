4daef5333c542c9f15c75dabc5ae7042a9b691cb
moulahi-mo <moulahi.mo@gmail.com>
2020-12-26 20:52:20 +0100

    adding animations

--- js/script.js
+++ js/script.js
@@ -13,21 +13,8 @@ innerWidth >= 576
   : false;
 
 $('body').scrollspy({ target: '#mainNav' });
-//* Smooth Scrolling
-// $('#mainNav a').on('click', function (event) {
-//   if (this.hash !== '') {
-//     event.preventDefault();
-
-//     const hash = this.hash;
-
-//     $('html, body').animate(
-//       {
-//         scrollTop: $(hash).offset().top,
-//       },
-//       800,
-//       function () {
-//         window.location.hash = hash;
-//       }
-//     );
-//   }
-// });
+//* wow animation
+const classAnim = 'wow animate__animated animate__fadeIn animate__slower';
+document.querySelectorAll('').forEach((col) => {
+  col.className = classAnim;
+});
